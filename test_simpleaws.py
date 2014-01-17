7cfc32ab1ca19b0b2fe174ac0880d51e398fd60c
Rich Jones <rich@anomos.info>
2014-01-17 14:18:35 -0800

    america eats its babies

--- test_simpleaws.py
+++ test_simpleaws.py
@@ -27,32 +27,40 @@ def main():
     simpleaws.set_keys(settings.AWS_ACCESS_KEY, settings.AWS_SECRET_ACCESS_KEY)
 
     print "Connecting: "
-    print simpleaws.connect()
+    success = simpleaws.connect()
+    if success:
+        print "Success!\n"
+    else:
+        print "Failure. :(\n"
+        return
 
     bucketname = str(uuid.uuid4())
-    print "Creating bucket " + bucketname + ": "
+    print "Creating bucket: " + bucketname + ": "
     bucket = simpleaws.create_bucket(bucketname)
-    print bucket
+    print "Success!\n"
 
-    username = str(uuid.uuid4())
+    username = 'user' + str(uuid.uuid4())
     print "Creating user: " + username
-    print simpleaws.create_user(username, bucketname)
+    user = simpleaws.create_user(username, bucketname)
+    print "Success! \n"
 
     print "Getting user keys: "
     keys = simpleaws.get_user_keys(username)
     print keys
+    print "\n"
 
-    print "Uploading test files to new bucket (As " + username + ":"
+    print "Uploading test files to new bucket as " + username + ":"
     contents = upload_test_files(username, bucket, bucketname, keys)
     print ''
-    print contents.generate_url(expires_in=300)
+    print "Success!"
+    print "File uploaded here: " + contents.generate_url(expires_in=300)
+    print "\n"
 
     print "Moving bucket to CloudFront"
     print simpleaws.move_bucket_to_cloudfront(bucketname)
 
     print "Backing up bucket"
     print simpleaws.backup_bucket(bucketname)
-
 
 if __name__ == "__main__":
     main()
